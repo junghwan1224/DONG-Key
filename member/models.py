5803a5f4a41ec0bc93729591edbdfb64b3269cdc
unknown <junghwan1224@hanmail.net>
2018-02-19 18:34:21 +0900

    '멤버_모델_정리'

--- member/models.py
+++ member/models.py
@@ -18,31 +18,3 @@ class Member(models.Model):
             default=False,
         )
     # positon False=member, True=admin
-
-
-class Attendance(models.Model):
-    member = models.ForeignKey(
-            Member,
-            on_delete=models.CASCADE,
-        )
-    attendance_check = models.BooleanField(
-            default=False,
-        )
-    attendance_date = models.DateTimeField(
-            auto_now_add=True,
-            verbose_name='attendance date',
-        )
-
-
-class Absence(models.Model):
-    attendance = models.OneToOneField(
-            Attendance,
-            on_delete=models.CASCADE,
-        )
-    minus = models.IntegerField(
-            default=0,
-        )
-    minus_reason = models.CharField(
-            max_length=100,
-            verbose_name='minus reason',
-        )
